40c339354a7a4f3e5af5d1545ead6b5f32622136
manoharmedapu <medapumanohar320@gmail.com>
2021-10-30 15:22:03 +0530

    add knife file

---
+++ src/main/java/kitchen/App.java
@@ -0,0 +1,13 @@
+package kitchen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
